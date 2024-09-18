f52619c705259c5ee036342d137050e8e8b63d03
Mikah-Ho-ICD2O1-02 <mikah.ho@student.tdsb.on.ca>
2024-09-18 17:21:06 +0000

    questions completed

--- PP1_3.py
+++ PP1_3.py
@@ -1,17 +1,33 @@
-
+'''
+Lesson: Using Strings
+Author: Mikah Ho
+Date Created: Sept 17, 2024
+Date Last Modified: Sept 18, 2024
+'''
 
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
+  print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *")
+  print()
+  print("* * * * *\n*       *\n*       *\n*       *\n* * * * *")
+  print()
+  print("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *")
+  print()
 
 def q2():
   #Write Assignment code here
+  print("Hello ", end = "")
+  print("World")
+  print()
   
 def q3():
   #Write Assignment code here
+  print("Man\nBoard\nMan Overboard")
   
 def q4():
   #Write Assignment code here
+  print()
 
 #Do not alter anything after this line
 q1()
